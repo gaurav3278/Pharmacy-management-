c7e1ec7160cd0fd54ad156f0992d8e0fb3886a23
Gaurav Rai <75034765+gaurav3278@users.noreply.github.com>
2022-06-19 12:00:54 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-img5 = Image.open("./pic1.jpg")
-        # img5 = img5.resize((150, 150), Image.ANTIALIAS)
-        # self.img5 = ImageTk.PhotoImage(img5)
-        # b3 = Button(self.root, image=self.img5, borderwidth=0)
-        # b3.place(x=1000, y=165)
